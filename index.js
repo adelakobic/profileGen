159294dd62af9d04069be039608883a0d01f7e11
adelakobic <alojzamauder@yahoo.co.uk>
2022-09-10 17:53:59 +0100

    function code added to index.js file but the terminal is pringting all the questions at the same time instead of one at a time - not sure how to fix this

--- index.js
+++ index.js
@@ -18,29 +18,61 @@ let employees = [];
 // start the questions
 function menu () {
     inquirer.prompt(MainMenu)
-    .then(answers=> {
+    .then(answers => {
         //if statement to send them to manager etc
+        if (response == addManager) {
+            return ManagerQuestions;
+
+        }   else if (response == addEngineer) {
+            return EngineerQuestions;
+
+        }   else (response == addIntern); {
+            return InternQuestions;
+        }
+    
     });
 }
 
 function addManager () {
     inquirer.prompt(ManagerQuestions)
     .then(answers=> {
-        console.log(answers)
+        console.log(answer)
         let manager = new Manger(answers.id, answers.name, answers.email, answers.telephone);
         employees.push(manager);
         menu();
     });
 }
 
-function addEngineer(){}
+function addEngineer(){
+    inquirer.prompt(EngineerQuestions)
+    .then(answers => {
+        console.log(answers)
+        let engineer = new Engineer(answers.id, answers.name, answers.email, answers.github);
+        employees.push(engineer);
+        menu();
+    })
+}
 
-function addIntern(){}
+function addIntern(){
+    inquirer.prompt(InternQuestions)
+    .then(answers => {
+        console.log(answers)
+        let intern = new Intern(answers.id, answers.name, answers.email, answers.school);
+        employees.push(intern);
+        menu();
+    })
+}
 
 function buildHTML() {
     //use FS here!
+    console.log(response);
+    const string = gernerateHtml(response);
+    fs.writeFile("index.html", string, (err) =>
+    err ? console.error(err) : console.log("Your profiles have been generated into HTML."))
 }
 
+inquirer.prompt(MainMenu).then(answers=> console.log(answers))
+inquirer.prompt(ManagerQuestions).then(answers=> console.log(answers))
 inquirer.prompt(EngineerQuestions).then(answers=> console.log(answers))
 inquirer.prompt(InternQuestions).then(answers=> console.log(answers))
 
